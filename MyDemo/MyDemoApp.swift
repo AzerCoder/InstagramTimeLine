1fde1c78520bf6999619c7f71d5458c4e8c27acf
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-12-04 20:53:10 +0500

    Initial Commit

---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by A'zamjon Abdumuxtorov on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
